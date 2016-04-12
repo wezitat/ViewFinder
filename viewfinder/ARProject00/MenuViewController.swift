e7e534cc58a5ac273485315be0c9c17b11738370
Ihor Zabrotsky <ihor.zabrotsky@techmagic.co>
2016-04-12 15:43:10 +0300

    MenuVC added

---
+++ viewfinder/ARProject00/MenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MenuViewController.swift
+//  ARProject00
+//
+//  Created by Ihor on 4/12/16.
+//  Copyright © 2016 Techmagic. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
